02a29c59984733590d586efd03c0ade72779be77
sauxpa <patrick.jr.saux@gmail.com>
2020-04-08 10:06:33 +0200

    add general case

--- rm_convergence_app/main.py
+++ rm_convergence_app/main.py
@@ -9,18 +9,25 @@ from bokeh.layouts import row, WidgetBox
 from bokeh.plotting import figure
 
 
-def make_dataset_wigner(N, gen_type, div_):
-    """Creates a ColumnDataSource object with data to plot.
-    """
+def gen_type_mgr(gen_type:int):
     if gen_type == 1:
         gen = uniform(
             loc=-uniform.mean(scale=1/uniform.std()),
             scale=1/uniform.std()
         )
         gen_type_str = 'Uniform'
-    else:
+    elif gen_type == 2:
         gen = norm
         gen_type_str = 'Gaussian'
+    else:
+        raise ValueError('Unknown generator type')
+    return gen, gen_type_str
+
+
+def make_dataset_wigner(N, gen_type, div_):
+    """Creates a ColumnDataSource object with data to plot.
+    """
+    gen, gen_type_str = gen_type_mgr(gen_type)
 
     assert gen.mean() == 0.0, 'Random generator should be centered.'
     assert gen.std() == 1.0, 'Random generator should be of unit variance.'
@@ -54,15 +61,7 @@ def make_dataset_wigner(N, gen_type, div_):
 def make_dataset_mp(N, n, gen_type, div_):
     """Creates a ColumnDataSource object with data to plot.
     """
-    if gen_type == 1:
-        gen = uniform(
-            loc=-uniform.mean(scale=1/uniform.std()),
-            scale=1/uniform.std()
-        )
-        gen_type_str = 'Uniform'
-    else:
-        gen = norm
-        gen_type_str = 'Gaussian'
+    gen, gen_type_str = gen_type_mgr(gen_type)
 
     assert gen.mean() == 0.0, 'Random generator should be centered.'
     assert gen.std() == 1.0, 'Random generator should be of unit variance.'
@@ -99,15 +98,7 @@ def make_dataset_mp(N, n, gen_type, div_):
 def make_dataset_large_cov(N, n, gen_type, eig1, freq1, eig2, freq2, eig3, freq3, div_):
     """Creates a ColumnDataSource object with data to plot.
     """
-    if gen_type == 1:
-        gen = uniform(
-            loc=-uniform.mean(scale=1/uniform.std()),
-            scale=1/uniform.std()
-        )
-        gen_type_str = 'Uniform'
-    else:
-        gen = norm
-        gen_type_str = 'Gaussian'
+    gen, gen_type_str = gen_type_mgr(gen_type)
 
     assert gen.mean() == 0.0, 'Random generator should be centered.'
     assert gen.std() == 1.0, 'Random generator should be of unit variance.'
@@ -148,6 +139,30 @@ def make_dataset_large_cov(N, n, gen_type, eig1, freq1, eig2, freq2, eig3, freq3
 
     # Convert dataframe to column data source
     return ColumnDataSource(df), ColumnDataSource(df_eig)
+
+
+def make_dataset_general(N, gen_type, div_):
+    """Creates a ColumnDataSource object with data to plot.
+    """
+    gen, gen_type_str = gen_type_mgr(gen_type)
+
+    assert gen.mean() == 0.0, 'Random generator should be centered.'
+    assert gen.std() == 1.0, 'Random generator should be of unit variance.'
+
+    A = gen.rvs(size=N**2).reshape(N, N) / np.sqrt(N)
+
+    spectrum = np.linalg.eigvals(A)
+    x, y = np.real(spectrum), np.imag(spectrum)
+
+    df = pd.DataFrame({'x': x,
+                       'y': y,
+                       })
+
+    params_text = '<b>Parameters:</b><br><ul><li>Entries distribution = {:s}</li></ul>'.format(gen_type_str)
+    div_.text = params_text
+
+    # Convert dataframe to column data source
+    return ColumnDataSource(df)
 
 
 def make_plot_wigner(src_wigner, src_pdf_wigner):
@@ -254,6 +269,26 @@ def make_plot_large_cov(src_large_cov, src_eig_large_cov):
     return fig_large_cov
 
 
+def make_plot_general(src_general):
+    """Create a figure object to host the plot.
+    """
+    # Blank plot with correct labels
+    fig_general = figure(plot_width=700,
+                         plot_height=700,
+                         title="General case of matrix rescaled by 1/sqrt(N)",
+                         )
+
+    theta = np.linspace(0, 2*np.pi, 100)
+    x_circle, y_circle = np.cos(theta), np.sin(theta)
+
+    fig_general.scatter('x', 'y', source=src_general, color='navy', alpha=0.7)
+    fig_general.line(x_circle, y_circle, color='red', alpha=0.9)
+
+    # fig_general.legend.click_policy = 'hide'
+    # fig_general.legend.location = 'top_right'
+    return fig_general
+
+
 def update_wigner(attr, old, new):
     """Update ColumnDataSource object.
     """
@@ -317,6 +352,20 @@ def update_large_cov(attr, old, new):
     src_eig_large_cov.data.update(new_src_eig_large_cov.data)
 
 
+def update_general(attr, old, new):
+    """Update ColumnDataSource object.
+    """
+    # Change to selected values
+    gen_type = gen_type_select_general.value
+    N = N_select_general.value
+
+    # Create new ColumnDataSource
+    new_src_general = make_dataset_general(N, gen_type, div_general)
+
+    # Update the data on the plot
+    src_general.data.update(new_src_general.data)
+
+
 ######################################################################
 ###
 ### WIGNER
@@ -331,11 +380,11 @@ gen_type_select_wigner = Slider(start=1,
                                )
 
 N_select_wigner = Slider(start=1,
-                        end=2000,
-                        step=1,
-                        value=500,
-                        title='Number of samples'
-                        )
+                         end=2000,
+                         step=1,
+                         value=500,
+                         title='Number of variables'
+                         )
 
 # Update the plot when parameters are changed
 gen_type_select_wigner.on_change('value', update_wigner)
@@ -359,6 +408,7 @@ layout_wigner = row(controls_wigner, fig_wigner)
 # Make a tab with the layout
 
 tab_wigner = Panel(child=layout_wigner, title='Wigner')
+
 
 ######################################################################
 ###
@@ -411,6 +461,7 @@ layout_mp = row(controls_mp, fig_mp)
 # Make a tab with the layout
 
 tab_mp = Panel(child=layout_mp, title='Marcenko-Pastur')
+
 
 ######################################################################
 ###
@@ -531,7 +582,52 @@ layout_large_cov = row(controls_large_cov, fig_large_cov)
 
 tab_large_cov = Panel(child=layout_large_cov, title='Large covariance matrix')
 
+
+######################################################################
+###
+### GENERAL CASE (Non-Hermitian matrix rescaled by 1/sqrt(N))
+###
+######################################################################
+# Slider to select parameters
+gen_type_select_general = Slider(start=1,
+                                 end=2,
+                                 step=1,
+                                 title='Entries distribution',
+                                 value=1,
+                                 )
+
+N_select_general = Slider(start=1,
+                          end=2000,
+                          step=1,
+                          value=500,
+                          title='Number of variables'
+                          )
+
+# Update the plot when parameters are changed
+gen_type_select_general.on_change('value', update_general)
+N_select_general.on_change('value', update_general)
+
+div_general = Div(text='<b>Parameters:</b><br>', width=300, height=100)
+
+src_general = make_dataset_general(N_select_general.value, gen_type_select_general.value, div_general)
+
+fig_general = make_plot_general(src_general)
+
+controls_general = WidgetBox(N_select_general,
+                             gen_type_select_general,
+                             div_general,
+                             width=300,
+                             )
+
+# Create a row layout
+layout_general = row(controls_general, fig_general)
+
+# Make a tab with the layout
+
+tab_general = Panel(child=layout_general, title='General case')
+
+
 ### ALL TABS TOGETHER
-tabs = Tabs(tabs=[tab_wigner, tab_mp, tab_large_cov])
+tabs = Tabs(tabs=[tab_wigner, tab_mp, tab_large_cov, tab_general])
 
 curdoc().add_root(tabs)
